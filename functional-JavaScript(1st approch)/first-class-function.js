85519ec473d15ca194696b5791fcf463078bf5ee
rafiswe <rafikulislam521@gmail.com>
2019-01-22 01:15:42 +0600

    created

---
+++ functional-JavaScript(1st approch)/first-class-function.js
@@ -0,0 +1,7 @@
+//Topics:
+//Function can be store in a variable.
+//Function can be store in a array.
+//Function can be store as an object.
+//We can create function as needed.
+//We can pass function as an argument.
+//We can return function from another function.
