c9395ed4aea95364b0d82a86c30c1ad53b3156a2
rafiswe <rafikulislam521@gmail.com>
2019-01-22 01:48:45 +0600

    6 topics has been cover about function

--- functional-JavaScript(1st approch)/first-class-function.js
+++ functional-JavaScript(1st approch)/first-class-function.js
@@ -1,7 +1,42 @@
 //Topics:
 //Function can be store in a variable.
+function sayName(name){
+    return 'Hello '+ name;
+}
+console.log(sayName('Rafi'));
+
+var result = sayName;
+var output = result('Rafi');
+console.log(output);
+
 //Function can be store in a array.
+var arr = [10, 20];
+arr.push(sayName('rafi'));
+console.log(arr);
+
 //Function can be store as an object.
-//We can create function as needed.
+var personDetails = {
+    firstName: 'Rafi',
+    lastName: 'Islam',
+    print: sayName("rafikul"),
+    output: function(){
+        return 'Hi all';
+    }
+}
+console.log(personDetails.print);
+console.log(personDetails.output());
+
+//We can create function as needed. It called invok function
+var sum = 50 + (function(){
+    return 10 + 15;
+})();
+console.log(sum);
+
 //We can pass function as an argument.
+function simplePrint(name, fun) {
+    return fun(name);
+}
+var result = simplePrint('Rafikul Islam', sayName);
+console.log(result);
+
 //We can return function from another function.
