da4056b1f276a36baf26a5cf3beea6ae3dc0ddb5
rafiswe <rafikulislam521@gmail.com>
2019-02-13 00:13:27 +0600

    map function details

--- functional-JavaScript(1st approch)/10-map-function.js
+++ functional-JavaScript(1st approch)/10-map-function.js
@@ -7,8 +7,26 @@ for (let i = 0; i < arr.length; i++) {
     }
 }
 console.log(newArr);
+
 //we can use map function for this work
 var newArray = arr.map(function(value) {
     return value + 2;
 })
 console.log(newArray);
+
+//we can make this map function by using callback function
+function myMap(arr, callback) {
+    var myNewMap = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        var data = callback(arr[i]);
+        myNewMap.push(data);
+    }
+    return myNewMap;
+}
+
+var myArr = myMap(arr, function(value) {
+    return value * 5;
+});
+
+console.log(myArr);
