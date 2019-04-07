86c5145f26c4e7493b6984b3039b2981212115b1
rafiswe <rafikulislam521@gmail.com>
2019-04-07 16:27:26 +0600

    Arrow function details

---
+++ ES-6/arrow-function.js
@@ -0,0 +1,12 @@
+function summation(a,b) {
+    return a+b;
+}
+console.log(summation(40,10));
+
+//Way in ES6
+const sum = (firstNumber, secondNumber) => firstNumber + secondNumber;
+console.log(sum(10, 20));
+
+const details = name => name;
+console.log(details("Rafi"));
+
