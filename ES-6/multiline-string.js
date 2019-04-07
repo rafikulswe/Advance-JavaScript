692864164f8b89b988b88ef4d2443fbb2e10c451
rafiswe <rafikulislam521@gmail.com>
2019-04-07 15:55:06 +0600

    string templating

---
+++ ES-6/multiline-string.js
@@ -0,0 +1,16 @@
+var name = "Md. Rafikul Islam";
+var age = 25;
+var education = "BSc in SWE";
+
+// var html = "My Name is "+name+". I am "
+//             +age+" years old."
+//             +" and I am graduated at "
+//             +education+".";
+
+var details = `My Name is ${name}.
+            I am ${age} years old. 
+            and I am graduated at 
+            ${education}.`
+            
+            //this cotation is contain above the tab button on your keyboard.
+console.log(details);
