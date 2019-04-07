5c6556f21b02fe0f9547d995337d05aa1de3ecb4
rafiswe <rafikulislam521@gmail.com>
2019-04-07 13:55:09 +0600

    es6 file uploaded

---
+++ ES-6/let-const-var.js
@@ -0,0 +1,16 @@
+function Demo() {
+    {
+        let age = 20;
+            {
+                const age = 35;
+                // age = 40; It's not Possible.
+                console.log(age);
+            }
+        age = 50; //It's Possible.
+        console.log(age);
+    }
+    // console.log(age); //let value can not retribe outside the let block.
+
+}
+Demo();
+
